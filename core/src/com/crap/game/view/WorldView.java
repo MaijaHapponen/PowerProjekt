19558e931a181c4aa8ef3aee836cc8cafe99f3f6
bzzandrea <bzzandrea@gmail.com>
2016-04-22 10:52:20 +0200

    Fixed so that world is visible

--- core/src/com/crap/game/view/WorldView.java
+++ core/src/com/crap/game/view/WorldView.java
@@ -3,20 +3,31 @@ package com.crap.game.view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.crap.game.model.Player;
 
 /**
  * Created by Maija on 2016-04-21.
+ * Edited by Andrea on 2016-04-22.
  */
 public class WorldView implements Screen {
 
+    private OrthographicCamera camera;
+    private Player player;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
     @Override
     public void show() {
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, width, height);
+        camera.update();
         map = new TmxMapLoader().load("maps/hubbeneditsand.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
     }
@@ -26,6 +37,8 @@ public class WorldView implements Screen {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        camera.update();
+        renderer.setView(camera);
         renderer.render();
     }
 

a000d95a40f63e60b32fc15d2b9c6811cb55512d
MaijaHapponen <maija.happonen.96@gmail.com>
2016-04-22 09:29:50 +0200

    some bug fixes and structure

--- core/src/com/crap/game/Main.java
+++ core/src/com/crap/game/Main.java
@@ -1,13 +1,15 @@
 package com.crap.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.crap.game.view.WorldView;
 
-public class Game extends com.badlogic.gdx.Game {
+public class Main extends Game {
 	
 	@Override
 	public void create () {
--- desktop/src/com/crap/game/desktop/DesktopLauncher.java
+++ desktop/src/com/crap/game/desktop/DesktopLauncher.java
@@ -2,14 +2,14 @@ package com.crap.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.crap.game.Game;
+import com.crap.game.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.useGL30 = true;
+        
         config.width=500;
         config.height=500;
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new Main(), config);
 	}
 }

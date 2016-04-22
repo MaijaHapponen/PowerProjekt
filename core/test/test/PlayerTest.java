51e619038946df69f71768c8e52ab604db20a04a
Lisa Larsson <lislar@student.chalmers.se>
2016-04-22 16:48:18 +0200

    fixes test classes

---
+++ core/test/test/PlayerTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by se on 22/04/16.
+ */
+public class PlayerTest {
+
+    @Test
+    public void testMoveUp() throws Exception {
+
+    }
+}

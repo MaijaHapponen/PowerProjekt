678a1523fef95a2ff2fb037d99b98aea113990ea
Lisa Larsson <lislar@student.chalmers.se>
2016-04-24 19:11:40 +0200

    Gives Mascot a position.

--- core/src/com/crap/game/model/Mascot.java
+++ core/src/com/crap/game/model/Mascot.java
@@ -4,4 +4,11 @@ package com.crap.game.model;
  * Created by Lisa on 24/04/16.
  */
 public class Mascot {
+
+    private Position position;
+
+    //Constructor
+    public Mascot(){
+        this.position = new Position();
+    }
 }

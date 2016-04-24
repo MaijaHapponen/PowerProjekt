79f692aa2b1358a5beb5b87b1e1e3f42fb10ed57
Lisa Larsson <lislar@student.chalmers.se>
2016-04-24 19:19:29 +0200

    New comments in the code.

--- core/src/com/crap/game/model/Position.java
+++ core/src/com/crap/game/model/Position.java
@@ -7,25 +7,30 @@ public class Position {
     private int x;
     private int y;
 
+    //Constructor
     public Position(){
         this.x = 0;
         this.x = 0;
     }
 
+    //Constructor
     public Position(int x, int y){
         this.x = x;
         this.y = y;
     }
 
+    //Sets the objects position.
     public void setPosition(int x, int y){
         this.x = x;
         this.y = y;
     }
 
+    //Returns the objects x-coordinate.
     public int getX(){
         return this.x;
     }
 
+    //Returns the objects y-coordinate.
     public int getY(){
         return this.y;
     }

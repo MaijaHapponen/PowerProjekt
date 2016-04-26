b876835c14fd2ce5d5cc539094ba9d0d3e3543c8
Lisa Larsson <lislar@student.chalmers.se>
2016-04-26 21:57:17 +0200

    New constructors in Human and Mascot.

--- core/src/com/crap/game/model/Mascot.java
+++ core/src/com/crap/game/model/Mascot.java
@@ -1,5 +1,7 @@
 package com.crap.game.model;
 
+import com.badlogic.gdx.graphics.Texture;
+
 /**
  * Created by Lisa on 24/04/16.
  */
@@ -13,5 +15,10 @@ public class Mascot extends Character{
     //Constructor
     public Mascot(Position position){
         super(position);
+    }
+
+    //Constructor
+    public Mascot(Position position, Texture texture){
+        super(position, texture);
     }
 }

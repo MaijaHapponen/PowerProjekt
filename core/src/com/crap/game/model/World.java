b005f0ec6f4832329d9333a3da0ff7d21301db8a
Lisa Larsson <lislar@student.chalmers.se>
2016-04-26 21:24:37 +0200

    methods for creating both humans and mascots

--- core/src/com/crap/game/model/World.java
+++ core/src/com/crap/game/model/World.java
@@ -10,6 +10,7 @@ public class World {
     private Progress progress;
     public static Player player;
     public static ArrayList<Human> humans = new ArrayList<Human>();
+    public static ArrayList<Mascot> mascots = new ArrayList<Mascot>();
     //public static CollisionModel collision;
     //public static TiledMap map;
     //private String level;
@@ -20,11 +21,24 @@ public class World {
         this.player = new Player();
         //this.collision = new CollisionModel();
 
+        createHumans();
+        createMascots();
+    }
+
+    //Populates an arrayList with humans.
+    public void createHumans(){
         for(int i=0; i<0; i++){
-            Position position = new Position(i*40, i*40); //TODO sen borde man nog ha en lista med charactärernas positioner.
+            Position position = new Position(i*50, i); //TODO sen borde man nog ha en lista med karaktärernas positioner.
             humans.add(new Human(position)); //TODO och en lista med deras "utseenden"
         }
+    }
 
+    //Populates an arrayList with mascots.
+    public void createMascots(){
+        for(int i=0; i<0; i++){
+            Position position = new Position(i, i*40); //TODO sen borde man nog ha en lista med karaktärernas positioner.
+            mascots.add(new Mascot(position)); //TODO och en lista med deras "utseenden"
+        }
     }
 
     //Checks if the game is over.

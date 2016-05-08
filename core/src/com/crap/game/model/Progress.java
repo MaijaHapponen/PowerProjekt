2b38468689334c59a8a61dd5259b8dfdfffca5ca
rebeccafinne <rebecca@finne.se>
2016-05-08 11:48:12 +0200

    A progressbar added as a pictur, will make it follow the player with same distance at all times.

--- core/src/com/crap/game/model/Progress.java
+++ core/src/com/crap/game/model/Progress.java
@@ -9,27 +9,50 @@ public class Progress {
 
     private ArrayList<Mascot> mascotsInGame = new ArrayList<Mascot>(); //TODO borde denna listan vara här eller i någon annan klass istället? Måste fylla på den när man startar spelet.
     private ArrayList<Mascot> mascotsCaught;
+    private Position position;
 
     //Constructor
-    public Progress(){
+    public Progress() {
         mascotsCaught = new ArrayList<Mascot>();
     }
 
     //Method checking if all mascots has been caught by the player.
-    public boolean areAllMascotsCaught(){
-        if(mascotsInGame.isEmpty()){
+    public boolean areAllMascotsCaught() {
+        if (mascotsInGame.isEmpty()) {
             return true;
         }
         return false;
     }
 
     //Adds the caught mascot to the list of mascots caught and removes it from the list of mascots still in the game.
-    public void mascotCaught(Mascot mascot){
-        for(int i=0; i<mascotsInGame.size(); i++){
-            if(mascotsInGame.get(i).equals(mascot)){
+    public void mascotCaught(Mascot mascot) {
+        for (int i = 0; i < mascotsInGame.size(); i++) {
+            if (mascotsInGame.get(i).equals(mascot)) {
                 mascotsInGame.remove(i);
             }
         }
         mascotsCaught.add(mascot);
+    }
+
+    public ArrayList<Mascot> getMascotsCaught() {
+        return this.mascotsCaught;
+    }
+
+    public ArrayList<Mascot> getMascotsInGame() {
+        return mascotsInGame;
+    }
+
+    public void charactersOnBar(ArrayList<Mascot> mascotsCaught) {
+        for (int i = 0; i < mascotsCaught.size(); i++) {
+            // if(mascotsCaught.get(i).getName().equals(g));
+
+
+            //  }
+        }
+
+    }
+
+    public Position getPosition(){
+        return this.position;
     }
 }

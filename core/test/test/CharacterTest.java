8a8bbe87333f68ce32833ad45c41de69fb56520f
rebeccafinne <rebecca@finne.se>
2016-05-16 12:09:18 +0200

    Added two new classes that will be filled in with tests

---
+++ core/test/test/CharacterTest.java
@@ -0,0 +1,22 @@
+package test;
+
+
+import static org.junit.Assert.*;
+
+import com.crap.game.model.Mascot;
+import org.junit.Test;
+/**
+ * Created by rebeccafinne on 2016-05-16.
+ */
+public class CharacterTest {
+
+
+    @Test
+    public void testSaySomething(){
+        //Character character = new Character("Tjo");
+
+
+    }
+
+
+}

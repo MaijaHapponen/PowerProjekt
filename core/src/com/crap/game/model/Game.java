9ed6e3b08bb97fd1288ea4cb9f5285956bc2dd95
bzzandrea <bzzandrea@gmail.com>
2016-05-18 16:56:02 +0200

    Entering and exiting worlds starting to work as it should - still magical

--- core/src/com/crap/game/model/Game.java
+++ core/src/com/crap/game/model/Game.java
@@ -38,7 +38,7 @@ public class Game {
     //Populates an arrayList with humans.
     public void createHumans(){
         for(int i=0; i<humansNames.length; i++){
-            Position position = new Position(i*100, i*50); //TODO setn borde man nog ha en lista med karaktärernas positioner.
+            Position position = new Position(i*100+50, i*50+50); //TODO setn borde man nog ha en lista med karaktärernas positioner.
             humans.add(new Human(humansNames[i], position));
         }
     }
@@ -46,7 +46,7 @@ public class Game {
     //Populates an arrayList with mascots.
     public void createMascots(){
         for(int i=0; i<mascotNames.length; i++){
-            Position position = new Position(i*50, i*100); //TODO sen borde man nog ha en lista med karaktärernas positioner.
+            Position position = new Position(i*50+50, i*100+50); //TODO sen borde man nog ha en lista med karaktärernas positioner.
             mascots.add(new Mascot(mascotNames[i], position));
         }
     }

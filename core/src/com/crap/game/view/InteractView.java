54a346dcf9be4e6a3d7257bf093f5c9f7e40a791
rebeccafinne <rebecca@finne.se>
2016-05-20 13:30:03 +0200

    New design added

--- core/src/com/crap/game/view/InteractView.java
+++ core/src/com/crap/game/view/InteractView.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -20,25 +21,23 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 public class InteractView implements Screen{
 
     private SpriteBatch batch;
-    private Batch drawableBatch;
     private Stage stage;
     private int worldWidth = 500;
     private int worldHeight = 500;
     private TextureRegionDrawable background;
-    TextureAtlas buttonAtlas;
-    BitmapFont font;
+
 
 
 
     private Skin skin;
+    private BitmapFont titleFont;
+    private BitmapFont font;
 
 
 
-    TextButton buttonPlay;
 
-    private Label questionLabel = new Label(String.format("This is a question"), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
-    //TODO get the strings from mascot through a get method
-    private Label answerLabel= new Label(String.format("Alternative answer"), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
+
+
 
 
 
@@ -46,33 +45,48 @@ public class InteractView implements Screen{
         batch = new SpriteBatch();
         skin = new Skin();
         background = new TextureRegionDrawable(new TextureRegion(new Texture("background/rectangle.png")));
-       // buttonAtlas = new TextureAtlas("assets/fonts/Candy Shop.ttf");
-       // font = new BitmapFont();
-
-
-
         create();
 
 
     }
 
     public void create(){
-        //skin.addRegions(buttonAtlas);
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator( Gdx.files.internal("fonts/Candy Shop.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size=20;
+        titleFont = generator.generateFont(parameter);
+        titleFont.setColor(Color.PINK);
+
+        parameter.size=10;
+        font= generator.generateFont(parameter);
+        generator.dispose();
+
+        Label questionLabel = new Label(String.format("This is a question"), new Label.LabelStyle(titleFont, Color.PINK));
+        Label answerLabel1= new Label(String.format("Alternative answer 1"), new Label.LabelStyle(font, Color.BLACK));
+        Label answerLabel2= new Label(String.format("Alternative answer 2"), new Label.LabelStyle(font, Color.BLACK));
+        Label answerLabel3= new Label(String.format("Alternative answer 3"), new Label.LabelStyle(font, Color.BLACK));
+        Label answerLabel4= new Label(String.format("Alternative answer 4"), new Label.LabelStyle(font, Color.BLACK));
+        //TODO get the strings from mascot through a get method
+
+
+
         Table table = new Table(skin);
         Viewport viewport = new FitViewport(worldHeight, worldWidth);
         this.stage = new Stage(viewport, batch);
-       // TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
-        //buttonStyle.font = font;
 
-        //buttonPlay = new TextButton("Play", buttonStyle);
 
-        table.setSize(500, 300);
         table.setBackground(background);
-        table.bottom();
-        table.padBottom(50);
+        table.center();
         table.add(questionLabel);
         table.row();
-        table.add(answerLabel);
+        table.add(answerLabel1);
+        table.row();
+        table.add(answerLabel2);
+        table.row();
+        table.add(answerLabel3);
+        table.row();
+        table.add(answerLabel4);
 
         table.setFillParent(true);
 

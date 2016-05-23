98e7de4ad0c116440a1b4b1b75f2526cba78ae01
rebeccafinne <rebecca@finne.se>
2016-05-23 20:21:07 +0200

    Crashing when using time and worl name is null

--- core/src/com/crap/game/view/InteractionView.java
+++ core/src/com/crap/game/view/InteractionView.java
@@ -17,41 +17,71 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 public class InteractionView extends ScreenAdapter{
 
     private Table table;
+    private Table welcomeTable;
     private Stage stage;
+    private Stage welcomeStage;
     private Viewport viewport;
     private int WorldWidth = 500;
     private int worldHeight = 500;
     private OrthographicCamera camera;
     private SpriteBatch batch;
 
+    private String worldName;
+
 
     private Label talkLable = new Label(String.format("Press SPACE to talk"), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-
+    private Label welcomeLabel = new Label(String.format("Welcome to " + worldName), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 
     public InteractionView(){
         batch = new SpriteBatch();
         viewport = new FitViewport(WorldWidth, worldHeight, new OrthographicCamera());
+        createInteraction();
+        createWelcome();
+    }
+
+    public void createInteraction(){
         stage = new Stage(viewport, batch);
         table = new Table();
         table.top();
         table.setFillParent(true);
         table.add(talkLable);
-        table.row();
         stage.addActor(table);
+    }
+
+    public void createWelcome(){
+        welcomeStage = new Stage(viewport, batch);
+        welcomeTable = new Table();
+        welcomeTable.top();
+        welcomeTable.setFillParent(true);
+        welcomeTable.add(welcomeLabel);
+        welcomeStage.addActor(welcomeTable);
     }
 
     @Override
     public void render(float delta) {
-        batch.setProjectionMatrix(stage.getCamera().combined);
-        stage.draw();
+
     }
 
     public Stage getStage(){
         return this.stage;
     }
 
+    public Stage getWelcomeStage(){
+        return this.welcomeStage;
+    }
     public OrthographicCamera getCamera(){
         return this.camera;
     }
+
+
+    public void setWorldName(String name){
+        this.worldName = name;
+    }
+
+    public void setWelcomeLabel(Label label){
+        this.welcomeLabel = label;
+    }
+
+
 
 }

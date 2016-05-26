8b7a6ab1b45429b11ac6d04b384bebc52e895bea
rebeccafinne <rebecca@finne.se>
2016-05-26 10:43:32 +0200

    Progresstest works

--- core/test/test/ProgressTest.java
+++ core/test/test/ProgressTest.java
@@ -13,16 +13,16 @@ import static org.junit.Assert.*;
 public class ProgressTest {
 
 
-   /* @Test
+    @Test
     public void testAreAllMascotsCaught(){
         Mascot mascot = new Mascot("hej");
         ArrayList<Mascot> inGame = new ArrayList<Mascot>();
         inGame.add(mascot);
         Progress progress = new Progress(inGame);
-        assertTrue(progress.areAllMascotsCaught());
+        assertTrue(progress.areAllMascotsCaught() == false);
         progress.mascotCaught(mascot);
         assertTrue(progress.areAllMascotsCaught());
-    }*/
+    }
 
     @Test
     public void testMascotsCaught(){

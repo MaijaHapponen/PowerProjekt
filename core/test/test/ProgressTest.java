6e804449e380bfa0b8f8bd1837cb8aeb255cf7c7
rebeccafinne <rebecca@finne.se>
2016-05-26 15:59:46 +0200

    Progress is now at 100 precent

--- core/test/test/ProgressTest.java
+++ core/test/test/ProgressTest.java
@@ -32,6 +32,33 @@ public class ProgressTest {
         Progress progress = new Progress(inGame);
         progress.mascotCaught(mascot);
         assertTrue(progress.getMascotsCaught().contains(mascot));
+        assertTrue(progress.newUpdate());
 
+    }
+
+    @Test
+    public void testResetProgress(){
+        Mascot mascot = new Mascot("HEJ");
+        ArrayList<Mascot> inGame = new ArrayList<Mascot>();
+        inGame.add(mascot);
+        Progress progress = new Progress(inGame);
+        progress.mascotCaught(mascot);
+        assertTrue(inGame.isEmpty());
+        progress.resetProgress();
+        assertFalse(inGame.isEmpty());
+    }
+
+    @Test
+    public void testSetMascotsInGame(){
+        Mascot mascot = new Mascot("Hej");
+        ArrayList<Mascot> inGame = new ArrayList<Mascot>();
+        inGame.add(mascot);
+        Progress progress = new Progress(inGame);
+        progress.mascotCaught(mascot);
+        Mascot mascot1 = new Mascot("TJO");
+        ArrayList<Mascot> newGame = new ArrayList<Mascot>();
+        newGame.add(mascot1);
+        progress.setMascotsInGame(newGame);
+        assertTrue(progress.getMascotsInGame() == newGame);
     }
 }

db057aceebd60155ebc78bc05e885a0fc7fc74fd
rebeccafinne <rebecca@finne.se>
2016-05-26 15:47:17 +0200

    Even more tests that works

--- core/test/test/MascotTest.java
+++ core/test/test/MascotTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import com.crap.game.model.Constants;
 import com.crap.game.model.Direction;
 import com.crap.game.model.Mascot;
 import org.junit.Test;
@@ -14,10 +15,23 @@ public class MascotTest {
     @Test
     public void testUpdateDirections(){
         Mascot mascot = new Mascot("hej");
-
         mascot.setWalkAwayDirection(Direction.DOWN);
         mascot.updateDirections();
         assertTrue(mascot.getWalkAwayDirection() == Direction.NO_DIRECTION);
 
+    }
+
+    @Test
+    public void testGettersAndSetters(){
+        Mascot mascot = new Mascot("HEJ");
+        mascot.setNormalSpeed();
+        assertTrue(mascot.getSpeed() == Constants.normalSpeed);
+        mascot.setCaught(true);
+        assertTrue(mascot.isCaught());
+        mascot.setWidthAndHeight(30, 40);
+        assertTrue(mascot.getWidth() == 30);
+        assertTrue(mascot.getHeight() == 40);
+        mascot.setSlowerSpeed();
+        assertTrue(mascot.getSpeed() == Constants.slowerSpeed);
     }
 }
